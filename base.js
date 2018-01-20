e1ac97d8672d287aabdba76d85dc3e1e7b368c2a
Brock Halladay <bhalladay@instructure.com>
2018-01-19 19:46:36 -0700

    When a project is entered, this script will grab the total time and the time for each category (tag) in Asana

--- base.js
+++ base.js
@@ -9,6 +9,50 @@ let token = `Bearer ${getToken}`;
 let projectUrl = `https://app.asana.com/api/1.0/projects/${projectID}/tasks`;
 
 let taskArr = [];
+let totalTime = 0.0;
+let procTime = 0.0;
+let instManagementTime = 0.0;
+let scriptTime = 0.0;
+let projectManagementTime = 0.0;
+let resourceGatheringTime = 0.0;
+let miscTime = 0.0;
+let quizzesTime = 0.0;
+let gaugeTime = 0.0;
+let meetingTime = 0.0;
+let ltiTime = 0.0;
+
+let categorizeTime = (time,id)=>{
+  if(id == "489809463131044"){
+    procTime = procTime + time;
+  }
+  if(id == "489809463131033"){
+    instManagementTime = instManagementTime + time;
+  }
+  if(id == "490010230308930"){
+    scriptTime = scriptTime + time;
+  }
+  if(id == "490020383917402"){
+    projectManagementTime = projectManagementTime + time;
+  }
+  if(id == "490020383917401"){
+    resourceGatheringTime = resourceGatheringTime + time;
+  }
+  if(id == "489900010718831"){
+    miscTime = miscTime + time;
+  }
+  if(id == "504887827798400"){
+    quizzesTime = quizzesTime + time;
+  }
+  if(id == "489809463131031"){
+    gaugeTime = gaugeTime + time;
+  }
+  if(id == "518968256082262"){
+    meetingTime = meetingTime + time;
+  }
+  if(id == "478952064986393"){
+    ltiTime = ltiTime + time;
+  }
+}
 
 let callPromise = new Promise((resolve, reject)=>{
   let getTaskIds = ()=>{
@@ -35,7 +79,7 @@ let callPromise = new Promise((resolve, reject)=>{
   getTaskIds();
 })
 
-let totalTime = (taskId)=>{
+let getTaskTime = (taskId)=>{
   return new Promise((resolve,reject)=>{
     req({
       url: `https://app.asana.com/api/1.0/tasks/${taskId}`,
@@ -47,7 +91,18 @@ let totalTime = (taskId)=>{
       if(error){
         reject(error);
       } else {
-        resolve(body);
+        console.log(body.data.tags);
+        let getAddedTime = body.data.custom_fields.filter((obj)=>{
+          return obj.id == '427581708538669';
+        });
+        let addedTime = getAddedTime[0].number_value;
+        if(addedTime != null){
+          categorizeTime(addedTime,body.data.tags[0].id);
+          resolve(addedTime);
+        } else {
+          resolve(0.0);
+        }
+        
       }
     });
   });
@@ -57,7 +112,27 @@ callPromise.then((response)=>{
   response.data.forEach((task)=>{
     taskArr.push(task.id);
   });
-  totalTime(taskArr[0]).then((body=>{
-    console.log(body.data.custom_fields);
-  })).catch((err)=>console.log(err));
+  taskArr.forEach((task,index, array)=>{
+    getTaskTime(task).then((time)=>{
+      totalTime = totalTime + time;
+      if(index === array.length - 1){
+        //setTimeout to put this call at the end of the execution stack
+        //Next step is to pass this data to a function that will create a line on a CSV file
+        setTimeout(()=>{
+          console.log(`Total Time: ${totalTime}`);
+          console.log(`Process Time: ${procTime}`);
+          console.log(`Canvas Instance Management Time: ${instManagementTime}`);
+          console.log(`Scripting Time: ${scriptTime}`);
+          console.log(`Project Management Time: ${projectManagementTime}`);
+          console.log(`Resource Gathering Time: ${resourceGatheringTime}`);
+          console.log(`Misc Time: ${miscTime}`);
+          console.log(`Quizzes Time: ${quizzesTime}`);
+          console.log(`Gauge Time: ${gaugeTime}`);
+          console.log(`Meeting/Calls Time: ${meetingTime}`);
+          console.log(`LTI Setup Time: ${ltiTime}`);
+        },1000);
+      }
+    })
+    .catch((err)=>console.log(err));
+  });
 }).catch((err)=>console.log(err));
